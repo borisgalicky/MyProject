522cdb09ca0989045c1a098468bddbeafb1701de
Boris Galický <boris.galicky@akademiasovy.sk>
2018-07-12 16:15:40 +0200

    added password length verification and improved server side

--- controllers/register.js
+++ controllers/register.js
@@ -1,10 +1,36 @@
-const db = require('../models/db');
+let db = require('../models/db');
+let express = require ("express");
+let app = express();
 
-exports.register_new_user = (req, res, next) => {
+exports.register_new_user = (req, res, next)=>{
     let firstname = req.body.fn_input;
     let lastname = req.body.ln_input;
     let username = req.body.username_input;
     let email = req.body.mail_input;
     let password = req.body.password_input;
     console.log(req.body);
+
+    let selectEmail = "select email from customers where email=?";
+    let selectUsername = "select username from customers where username=?";
+    let insertUser = 'insert into customers(FirstName,LastName,Username,Email,Password,DateOfRegistration) values(?,?,?,?,?,CURRENT_TIMESTAMP())';
+
+    db.query(selectEmail,[email],(err, result)=>{
+    if(err) return next(err);
+        if(result.length == 0) {
+            db.query(selectUsername,[username],(err,rslt)=>{
+                if (err) return next(err);
+                if(rslt.length == 0){
+                    db.query(insertUser,[firstname,lastname,username,email,password],(err)=>{
+                        if (err) return next(err);
+                        console.log('USER REGISTERED!');
+                    });
+                }else{
+                    console.log('USER ALREADY EXISTS!');
+                }
+                
+            });
+        }else{
+            console.log('USER ALREADY EXISTS!');
+        }
+    });
 }
--- js/registration.js
+++ js/registration.js
@@ -14,6 +14,7 @@ function register(){
     let inputs = new Array(firstname,lastname,email,username,password,passwordconf);
     let errors = new Array(fn_err,ln_err,email_err,username_err,password_err,passwordconf_err);
     let empty=0;
+    //zisťovanie počtu prázdnch inputov
     for(let i=0;i<6;i++){
         if(inputs[i] == ''){
             empty+=1;
@@ -22,25 +23,32 @@ function register(){
         }
     }if(empty==0){
         if(password == passwordconf){
-            //vynulovanie všetkých error hlášok
-            //console.log("inputs OK, passwords matches!");
-            for(let i=0;i<6;i++){
-                errors[i].html("");
+            if((password.length>5) && (password.length<31)){
+                //vynulovanie všetkých error hlášok
+                for(let i=0;i<6;i++){
+                    errors[i].html("");
+                }
+            location.reload();
+            }if(password.length<6){
+                password_err.html("Too short password!");
+                passwordconf_err.html("");
+                return false;
+            }if(password.length>30){
+                password_err.html("Too long password!");
+                passwordconf_err.html("");
+                return false;
             }
         }else{
             //vynulovanie všetkých error hlášok okrem hlášky pri confirm password
             for(let i=0;i<5;i++){
                 errors[i].html("");
             }
-            //console.log("inputs OK but passwords does not match!");
             passwordconf_err.html("Passwords are not matching!");
             return false;
         }
-        
     }
     //vypísanie error hlášok ku prázdnym inputom
     else{
-        //console.log("not ok");
         for(let i=0;i<6;i++){
             if(inputs[i] == ''){
                 errors[i].html("Enter value!");
